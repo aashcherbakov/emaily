d4a4c05094de60e050794cfa92fdcf1af3600bea
Alex Shcherbakov <alexander@prolificinteractive.com>
2018-01-17 19:10:56 -0800

    Setup Redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import App from "./components/App";
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
