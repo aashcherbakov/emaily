105d71428eae16c51bafb0fecaa43cbae8d2d1f7
Alex Shcherbakov <alexander@prolificinteractive.com>
2018-02-03 11:41:39 -0800

    Add get surveys redux setup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
+import { FETCH_SURVEYS } from "./types";
 
 // to forward calls to the server, index.js has to have another proxy rule with /api/* in it
 
@@ -18,4 +19,9 @@ export const submitSurvey = (values, history) => async dispatch => {
   const response = await axios.post("/api/surveys", values);
   history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: response.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const response = await axios.get("/api/surveys");
+  dispatch({ type: FETCH_SURVEYS, payload: response.data });
 };
