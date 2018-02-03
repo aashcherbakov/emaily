86cdb85a12516fd9d6bab262f1a121f453b7bb71
Alex Shcherbakov <alexander@prolificinteractive.com>
2018-02-03 12:18:27 -0800

    Display survey results

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SurveyList from "./Surveys/SurveyList";
 
 const Dashboard = () => {
   return (
     // must edit index.html in public folder to access icons: <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <div>
-      Dashboard
+      <SurveyList />
       <div className="fixed-action-btn">
         <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="large material-icons">chat_bubble_outline</i>
